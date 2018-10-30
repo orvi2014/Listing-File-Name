d8887a49581efd6ac548f66e1a7d2d67dc05b0ba
Kulsum Siddique <kulsumsiddiquenova@gmail.com>
2018-10-30 14:31:40 +0600

    First commit

---
+++ listing_dir.py
@@ -0,0 +1,19 @@
+import os,sys
+import codecs
+
+path = 'C:\\Users\\KULSUM\\Desktop\\DigiJadoo_18-10-27-0826'
+mylist = os.listdir(path)
+
+for file in mylist:
+
+  data = file.replace(".xml", "\n")
+  data_modified = data.replace("_", " ")
+
+  file = codecs.open("testfile.txt","+a","utf-8")
+  file.write(data_modified)
+  file.close()
+
+
+
+  
+   
